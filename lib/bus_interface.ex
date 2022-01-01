50e025c5fe471c1e90afab38a967fac0ba63c62b
Clay Sampson <pdgeek@gmail.com>
2022-01-01 08:50:32 -0500

    wiring for but interface api

--- lib/bus_interface.ex
+++ lib/bus_interface.ex
@@ -1,13 +1,27 @@
 defmodule RtlSdrHomebridge.BusInterface do
   @moduledoc """
-  Interface to the messaging bus between the RTL-SDR and Homebridge
+  Interface to the messaging bus between the RTL-SDR and Homebridge. Message field transformation happens here.
   """
   use GenServer
   use QolUp.LoggerUtils
 
+  @type ok_or_err :: :ok | {:error, any()}
+
   ##############################
   # API
   ##############################
+
+  @spec pub_zone_state(non_neg_integer(), String.t(), integer(), integer()) :: ok_or_err()
+  @doc """
+  Publish a zone state update
+  ## Parameters
+  - `zone_id` The zone id (e.g. 125008)
+  - `zone_state` The zone state (e.g. "closed")
+  - `zone_tamper` The zone tamper status (e.g. 1 for tamper, 0 for not tampered)
+  - `zone_battery_ok` The battery ok status (e.g. 1 for ok, 0 for battery not ok)
+  """
+  def pub_zone_state(zone_id, zone_state, zone_tamper, zone_battery_ok),
+    do: GenServer.call(__MODULE__, {:pub_zone_state, zone_id, zone_state, zone_tamper, zone_battery_ok})
 
   @doc """
   Start the worker
@@ -34,7 +48,20 @@ defmodule RtlSdrHomebridge.BusInterface do
     {:ok, %State{}}
   end
 
+  @impl GenServer
+  def handle_call({:pub_zone_state, zone_id, zone_state, zone_tamper, zone_battery_ok}, _from, state) do
+    {updated_state, result} = do_pub_zone_state(state, zone_id, zone_state, zone_tamper, zone_battery_ok)
+    {:reply, result, updated_state}
+  end
+
   ##############################
   # Internal Calls
   ##############################
+
+  @spec do_pub_zone_state(State.t(), non_neg_integer(), String.t(), integer(), integer()) :: {State.t(), ok_or_err()}
+  @doc false
+  def do_pub_zone_state(state, zone_id, zone_state, zone_tamper, zone_battery_ok) do
+    L.locals()
+    {state, :ok}
+  end
 end
