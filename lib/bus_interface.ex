abf2d611e4187aad4ac0cac7faeb5b2b070e856a
Clay Sampson <pdgeek@gmail.com>
2022-01-02 10:26:14 -0500

    mqtt interface working as properly as it can given buggy mqttthing

--- lib/bus_interface.ex
+++ lib/bus_interface.ex
@@ -24,18 +24,36 @@ defmodule RtlSdrHomebridge.BusInterface do
   - `{:error, reason}` Failed for reason
   """
   def pub_zone_state(kind, zone_id, zone_state, zone_tamper, zone_battery_ok) do
-    L.locals()
     :ok = RtlSdrHomebridge.DeviceAccumulator.Worker.seen(kind, zone_id)
 
-    state = if zone_state == "closed", do: "1", else: "0"
-    tamper = "#{zone_tamper}"
-    low_battery = if zone_battery_ok == 1, do: "0", else: "1"
+    # The mqttthing plugin does some weird stuff here:
+    # state
+    #   false = closed
+    #   true = open
+    # tamper
+    #   true = tampered
+    #   false = not tampered
+    #   but the homebridge logs complain "not an integer" if you send "true" or "false". Yet, if
+    #   you send "1" or "0", the accessory state doesn't get updated. So: send true/false to get
+    #   the accessory state updated and put up with the logs complaining that the value you
+    #   sent wasn't an integer. Except integers are ignored. Sigh.
+    # low_battery
+    #   true = low battery
+    #   false = battery okay
+    #   read the comments about tamper; low_battery has the same dumb issue. Sigh.
+
+    state = if zone_state == "closed", do: "false", else: "true"
+    tamper = if zone_tamper == 1, do: "true", else: "false"
+    low_battery = if zone_battery_ok == 1, do: "false", else: "true"
     opts = [qos: 0, timeout: 1_000]
+    base_topic = "rtl-sdr/#{kind}/#{zone_id}"
+
+    L.di(%{tamper: tamper, state: state, low_battery: low_battery, base_topic: base_topic})
 
     try do
-      with :ok <- Tortoise.publish(RtlSdrHomebridge, "rtl-sdr/#{kind}/#{zone_id}/state", state, opts),
-           :ok <- Tortoise.publish(RtlSdrHomebridge, "rtl-sdr/#{kind}/#{zone_id}/tamper", tamper, opts),
-           :ok <- Tortoise.publish(RtlSdrHomebridge, "rtl-sdr/#{kind}/#{zone_id}/low_battery", low_battery, opts) do
+      with :ok <- Tortoise.publish(RtlSdrHomebridge, "#{base_topic}/state", state, opts),
+           :ok <- Tortoise.publish(RtlSdrHomebridge, "#{base_topic}/tamper", tamper, opts),
+           :ok <- Tortoise.publish(RtlSdrHomebridge, "#{base_topic}/low_battery", low_battery, opts) do
         :ok
       else
         error ->
