3984d355b688b8bc3c7b47f8348b46bcf86d1d58
Clay Sampson <pdgeek@gmail.com>
2022-01-01 22:33:18 -0500

    remove unused configuration map

--- config/runtime.exs
+++ config/runtime.exs
@@ -4,10 +4,5 @@ config :rtl_sdr_homebridge,
   device_map: %{
     0 => "honeywell_345"
   },
-  contact_sensor_map: %{
-    49183 => %{
-      name: "Deck Door"
-    }
-  },
   mqtt_host: "192.168.58.150",
   mqtt_port: 1883
